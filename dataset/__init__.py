5beb957f0850746b70009407a241cc128a2aa026
Miao Li <miaoli.cs@gmail.com>
2022-04-28 17:56:20 +1000

    update

---
+++ dataset/__init__.py
@@ -0,0 +1,3 @@
+# Please download PeerSum dataset into this folder
+# Google Drive: https://drive.google.com/drive/folders/1M1QhIwjuZOG3QdxNFqY7J5Ik5UsDA0Sk?usp=sharing
+# Baidu Netdisk: https://pan.baidu.com/s/1fleJ4MXcTQ2PYmlbJ8tDBA?pwd=s3wi
